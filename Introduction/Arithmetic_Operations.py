82eebd1b552343cdfc64297604dbc5bca3905033
Mehmet Polat <mehmet.polat@ogr.gidatarim.edu.tr>
2025-03-09 01:17:13 +0300

    Update Arithmetic_Operations.py

--- Introduction/Arithmetic_Operations.py
+++ Introduction/Arithmetic_Operations.py
@@ -2,6 +2,6 @@ a = int(input()) # First input is 3 in HackerRank
 b = int(input()) # Second input is 5 in HackerRank
  
            # Expected outputs
-print(a+b) # 5   
+print(a+b) # 8  
 print(a-b) # 1
 print(a*b) # 6
