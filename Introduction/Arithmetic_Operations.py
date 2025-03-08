f8a2cc59afdb17e7ce052b431a93dcee3af1d341
Mehmet Polat <mehmet.polat@ogr.gidatarim.edu.tr>
2025-03-09 00:50:01 +0300

    Add files via upload

---
+++ Introduction/Arithmetic_Operations.py
@@ -0,0 +1,7 @@
+a = int(input()) # First input is 3 in HackerRank
+b = int(input()) # Second input is 5 in HackerRank
+ 
+           # Expected outputs
+print(a+b) #5   
+print(a-b) #1
+print(a*b) #6
